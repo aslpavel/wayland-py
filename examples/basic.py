e6805befa40edd43f2daeaeae0c89d0f5a16530c
Pavel Aslanov <asl.pavel@gmail.com>
2021-11-17 21:24:03 +0000

    [examples] cleanup basic example

--- examples/basic.py
+++ examples/basic.py
@@ -1,10 +1,35 @@
 #!/usr/bin/env python
 import asyncio
-from typing import Any, Callable
+from typing import Optional, Tuple
 from wayland.client import ClientConnection
-from wayland.protocol.wayland import WlShm, WlCompositor
+from wayland.protocol.wayland import WlBuffer, WlShm, WlCompositor
 from wayland.protocol.xdg_shell import XdgWmBase
 from wayland import SharedMemory
+
+
+def draw(wl_shm: WlShm, width: int, height: int) -> WlBuffer:
+    stride = width * 4
+    size = stride * height
+    buf_mem = SharedMemory(size)
+
+    for y in range(height):
+        for x in range(width):
+            offset = y * stride + x * 4
+            if (x + int(y / 8) * 8) % 16 < 8:
+                buf_mem.buf[offset : offset + 4] = b"\x66\x66\x66\xff"
+            else:
+                buf_mem.buf[offset : offset + 4] = b"\xee\xee\xee\xff"
+
+    with wl_shm.create_pool(buf_mem, size) as pool:
+        buf = pool.create_buffer(0, width, height, stride, WlShm.Format.XRGB8888)
+
+    @buf.on_release
+    def _() -> bool:
+        buf.destroy()
+        buf_mem.close()
+        return False
+
+    return buf
 
 
 async def main() -> None:
@@ -14,60 +39,47 @@ async def main() -> None:
     wl_compositor = conn.get_global(WlCompositor)
     xdg_wm_base = conn.get_global(XdgWmBase)
 
-    # still alive
-    def pong(serial: int) -> bool:
-        print("ping")
-        xdg_wm_base.pong(serial)
-        return True
-
-    xdg_wm_base.on_ping(pong)
-
     # surface
     wl_surf = wl_compositor.create_surface()
     xdg_surf = xdg_wm_base.get_xdg_surface(wl_surf)
     xdg_toplevel = xdg_surf.get_toplevel()
-    xdg_toplevel.set_title("wayland-py")
+    xdg_toplevel.set_title("wayland-py-basic")
     wl_surf.commit()
 
-    # memory buffer
-    width = 640
-    height = 480
-    stride = width * 4
-    size = stride * height
-    buf_mem = SharedMemory(size)
+    # handlers
+    @xdg_wm_base.on_ping
+    def _(serial: int) -> bool:
+        xdg_wm_base.pong(serial)
+        return True
 
-    # draw
-    for y in range(height):
-        for x in range(width):
-            offset = y * stride + x * 4
-            if (x + int(y / 8) * 8) % 16 < 8:
-                buf_mem.buf[offset : offset + 4] = b"\x66\x66\x66\xff"
-            else:
-                buf_mem.buf[offset : offset + 4] = b"\xee\xee\xee\xff"
+    @xdg_toplevel.on_close
+    def _() -> bool:
+        conn.terminate()
+        return True
 
-    # create wl_buffer
-    with wl_shm.create_pool(buf_mem, size) as pool:
-        buf = pool.create_buffer(0, width, height, stride, WlShm.Format.XRGB8888)
+    size: Optional[Tuple[int, int]] = None
 
-    # handle updates
-    def on_configure(serial: int) -> bool:
-        print("draw")
-        wl_surf.attach(buf, 0, 0)
+    @xdg_toplevel.on_configure
+    def _(new_width: int, new_height: int, _: bytes) -> bool:
+        nonlocal size
+        new_size: Optional[Tuple[int, int]] = None
+        if size is None:
+            new_size = (new_width or 640, new_height or 480)
+        elif new_width != 0 and new_height != 0 and size != (new_width, new_height):
+            new_size = (new_width, new_height)
+        if new_size is not None:
+            size = new_size
+            width, height = size
+            wl_surf.attach(draw(wl_shm, width, height), 0, 0)
+        return True
+
+    @xdg_surf.on_configure
+    def _(serial: int) -> bool:
         xdg_surf.ack_configure(serial)
         wl_surf.commit()
         return True
 
-    xdg_surf.on_configure(on_configure)
-
     await conn.on_terminated()
-
-
-def print_message(msg: str) -> Callable[..., bool]:
-    def print_message_handler(*args: Any) -> bool:
-        print(msg, args)
-        return True
-
-    return print_message_handler
 
 
 if __name__ == "__main__":
